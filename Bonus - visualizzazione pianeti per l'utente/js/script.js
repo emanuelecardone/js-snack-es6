1dd094f798c7e69bfd2a4db428e592d9e1c67ee5
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-26 14:52:02 +0100

    Creata cartella bonus (da iniziare)

---
+++ Bonus - visualizzazione pianeti per l'utente/js/script.js
@@ -0,0 +1,11 @@
+// Obiettivi
+//
+// Creare uno slider a scorrimento contenente tutti i pianeti del sistema solare (preso spunto da un esercizio a lezione)
+// Per ogni pianeta, verranno visualizzati: nome, massa, temperatura, satelliti, diametro equatoriale, velocità orbitale
+// L'utente ha la possibilità di scorrere i pianeti e visualizzare ogni caratteristica di essi 
+// Vedendo anche stampata una foto in pagina che corrisponde al pianeta attivo
+// Ci sarà poi una lista dei pianeti più piccola in alto, e anche lì l'active sarà evidenziato
+// Ci saranno 3 sfondi diversi a tema spazio, l'utente potrà cambiare sfondo quando vuole
+// Ogni pianeta deve avere una sezione "About" che appare/scompare al click nella quale si parla del pianeta con info aggiunte
+// L'utente può cambiare nome a ciascun pianeta attivo quando vuole
+                                
