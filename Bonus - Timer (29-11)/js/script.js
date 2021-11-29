e20c2bfcdc32a397ba999a6a8db65794187edb82
emanuelecardone <e_m.cardone@yahoo.it>
2021-11-29 15:30:55 +0100

    Creata cartella per bonus timer (da fare)

---
+++ Bonus - Timer (29-11)/js/script.js
@@ -0,0 +1,10 @@
+// Variabile body
+const pageBody = document.querySelector('body');
+
+// Variabile header, main e footer 
+const pageHeader = document.createElement('header');
+const pageMain = document.createElement('main');
+const pageFooter = document.createElement('footer');
+
+// Classi di stile header, main e footer
+pageHeader.classList.add('w-100', 'h_100p');
